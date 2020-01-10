03e90a7d90a69224e3dca50b130e489a3c080f49
Jane Njeri <aquilanjeri@gmail.com>
2020-01-10 16:55:25 +0300

    Analysis of PDB files

---
+++ Scripts/.ipynb_checkpoints/Launcher-checkpoint.py
@@ -0,0 +1,13 @@
+#! usr/bin/python
+
+
+""" This section initializes the program,and calls in the Mainmenu """
+
+# No pdb file at start
+pdb = "None"
+
+# Importing the start module
+from Menu import Mainmenu
+
+# Initializing the program by calling the Mainmenu function
+Mainmenu()
